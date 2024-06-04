aa20ab6b3a504f71d158cbb140f104737b34579e
hadi77738 <ahmad.hadi77738@gmail.com>
2024-06-04 20:09:53 +0700

    update

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,262 @@
+@extends('layouts.frontend')
+
+@section('content')
+<!--==================== HOME ====================-->
+<section>
+    <div class="swiper-container">
+        <div>
+            <!--========== ISLANDS 1 ==========-->
+            <section class="islands">
+                <img
+                    src="{{ asset('frontend/assets/img/index.jpg') }}"
+                    alt=""
+                    class="islands__bg"
+                />
+                <div class="bg__overlay">
+                    <div class="islands__container container">
+                    <div
+                            class="islands__data"
+                            style="z-index: 99; position: relative; color: white; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;"
+                        >
+                            <h2 class="islands__subtitle">
+                                Telusuri
+                            </h2>
+                            <h1 class="islands__title">
+                                WISATA RELIGI
+                            </h1>
+                            <p class="islands__description">
+                                Embark on a sacred journey and<br />
+                                experience the divine beauty of<br />
+                                the world's most revered places.
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</section>
+
+<!--==================== LOGOS ====================-->
+<section
+    class="logos"
+    style="margin-top: 9rem; padding-bottom: 3rem"
+>
+    <div class="logos__container container grid">
+        <div class="logos__img">
+            <img src="{{ asset('frontend/assets/img/sp1.jpg') }}" alt="" />
+        </div>
+        <div class="logos__img">
+            <img src="{{ asset('frontend/assets/img/sp2.png') }}" alt="" />
+        </div>
+        <div class="logos__img">
+            <img src="{{ asset('frontend/assets/img/sp3.png') }}" alt="" />
+        </div>
+        <div class="logos__img">
+            <img src="{{ asset('frontend/assets/img/sp4.png') }}" alt="" />
+        </div>
+    </div>
+</section>
+
+<!--==================== POPULAR ====================-->
+<section class="section" id="popular">
+    <div class="container">
+        <span class="section__subtitle" style="text-align: center"
+            >Best Choice</span
+        >
+        <h2 class="section__title" style="text-align: center">
+            Popular Places
+        </h2>
+
+        <div class="popular__container swiper">
+            <div class="swiper-wrapper">
+                @foreach($travel_packages as $travel_package)
+                    <article class="popular__card swiper-slide">
+                        <a href="{{ route('travel_package.show', $travel_package->slug) }}">
+                            <img
+                                src="{{ Storage::url($travel_package->galleries->first()->images) }}"
+                                alt=""
+                                class="popular__img"
+                            />
+                            <div class="popular__data">
+                                <h2 class="popular__price">
+                                    <span>$</span>{{ number_format($travel_package->price,2) }}
+                                </h2>
+                                <h3 class="popular__title">
+                                    {{ $travel_package->location}}
+                                </h3>
+                                <p class="popular__description">{{ $travel_package->type }}</p>
+                            </div>
+                        </a>
+                    </article>
+                @endforeach
+            </div>
+
+            <div class="swiper-button-next">
+                <i class="bx bx-chevron-right"></i>
+            </div>
+            <div class="swiper-button-prev">
+                <i class="bx bx-chevron-left"></i>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--==================== VALUE ====================-->
+<section class="value section" id="value">
+    <div class="value__container container grid">
+        <div class="value__images">
+            <div class="value__orbe"></div>
+
+            <div class="value__img">
+                <img src="{{ asset('frontend/assets/img/team.jpg') }}" alt="" />
+            </div>
+        </div>
+
+        <div class="value__content">
+            <div class="value__data">
+                <span class="section__subtitle">Kenapa Harus Memilih Kami?</span>
+                <h2 class="section__title">
+                    Pengalaman Apa Yang Akan Anda Dapatkan
+                </h2>
+                <p class="value__description">
+                Kami selalu siap melayani Anda dengan memberikan pelayanan terbaik. 
+                Kami menawarkan pilihan perjalanan religi yang sempurna ke berbagai destinasi di seluruh dunia.
+                </p>
+            </div>
+
+            <div class="value__accordion">
+                <div class="value__accordion-item">
+                    <header class="value__accordion-header">
+                        <i
+                            class="bx bxs-shield-x value-accordion-icon"
+                        ></i>
+                        <h3 class="value__accordion-title">
+                            Lokasi ziarah terbaikdiberbagai dunia
+                        </h3>
+                        <div class="value__accordion-arrow">
+                            <i class="bx bxs-down-arrow"></i>
+                        </div>
+                    </header>
+
+                    <div class="value__accordion-content">
+                        <p class="value__accordion-description">
+                            We provides the best places around the
+                            world and have a good quality of
+                            service.
+                        </p>
+                    </div>
+                </div>
+                <div class="value__accordion-item">
+                    <header class="value__accordion-header">
+                        <i
+                            class="bx bxs-x-square value-accordion-icon"
+                        ></i>
+                        <h3 class="value__accordion-title">
+                            Harga terbaik untuk anda
+                        </h3>
+                        <div class="value__accordion-arrow">
+                            <i class="bx bxs-down-arrow"></i>
+                        </div>
+                    </header>
+
+                    <div class="value__accordion-content">
+                        <p class="value__accordion-description">
+                            We try to make your budget fit with the
+                            destination that you want to go.
+                        </p>
+                    </div>
+                </div>
+                <div class="value__accordion-item">
+                    <header class="value__accordion-header">
+                        <i
+                            class="bx bxs-bar-chart-square value-accordion-icon"
+                        ></i>
+                        <h3 class="value__accordion-title">
+                            Rencanakan liburan anda
+                        </h3>
+                        <div class="value__accordion-arrow">
+                            <i class="bx bxs-down-arrow"></i>
+                        </div>
+                    </header>
+
+                    <div class="value__accordion-content">
+                        <p class="value__accordion-description">
+                            Provides you with time properly.
+                        </p>
+                    </div>
+                </div>
+                <div class="value__accordion-item">
+                    <header class="value__accordion-header">
+                        <i
+                            class="bx bxs-check-square value-accordion-icon"
+                        ></i>
+                        <h3 class="value__accordion-title">
+                            Jaminan keamanan
+                        </h3>
+                        <div class="value__accordion-arrow">
+                            <i class="bx bxs-down-arrow"></i>
+                        </div>
+                    </header>
+
+                    <div class="value__accordion-content">
+                        <p class="value__accordion-description">
+                            We make sure that our services have a
+                            good of security
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- blog -->
+<section class="blog section" id="blog">
+    <div class="blog__container container">
+        <span class="section__subtitle" style="text-align: center"
+            >Our Blog</span
+        >
+        <h2 class="section__title" style="text-align: center">
+            The Best Trip For You
+        </h2>
+
+        <div class="blog__content grid">
+            @foreach($blogs as $blog)
+                <article class="blog__card">
+                    <div class="blog__image">
+                        <img
+                            src="{{ Storage::url($blog->image) }}"
+                            alt=""
+                            class="blog__img"
+                        />
+                        <a href="{{ route('blog.show', $blog->slug) }}" class="blog__button">
+                            <i class="bx bx-right-arrow-alt"></i>
+                        </a>
+                    </div>
+
+                    <div class="blog__data">
+                        <h2 class="blog__title">
+                            {{ $blog->title }}
+                        </h2>
+                        <p class="blog__description">
+                            {{ $blog->excerpt }}
+                        </p>
+
+                        <div class="blog__footer">
+                            <div class="blog__reaction">
+                                {{ date('d M Y', strtotime($blog->created_at)) }}
+                            </div>
+                            <div class="blog__reaction">
+                                <i class="bx bx-show"></i>
+                                <span>{{ $blog->reads }}</span>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            @endforeach
+        </div>
+    </div>
+</section>
+@endsection
