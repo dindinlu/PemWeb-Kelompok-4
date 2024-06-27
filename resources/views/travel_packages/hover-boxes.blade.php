b49ec279ad8e2df590047f3bc7dc8866d52c10d7
dindinlu <165020077+dindinlu@users.noreply.github.com>
2024-06-27 21:57:36 +0700

    boxes travel package

---
+++ resources/views/travel_packages/hover-boxes.blade.php
@@ -0,0 +1,17 @@
+<div class="hover-box">
+    <div class="box">
+        <h2>Package Wisata 1 Kota</h2>
+        <p>
+            Nikmati liburan di Bali dengan paket lengkap!
+        </p>
+    </div>
+</div>
+
+<div class="hover-box">
+    <div class="box">
+        <h2>Package Wisata Lebih dari 1 Kota</h2>
+        <p>
+            Jelajahi Indonesia dari Sabang sampai Merauke!
+        </p>
+    </div>
+</div>
