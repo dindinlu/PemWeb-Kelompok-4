1901392072cee936dc522cbb69869ceaeb54019e
Ratnacempakamanao31 <ratnamanao31@gmail.com>
2024-07-03 00:01:24 +0700

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,18 +7,30 @@
         <div>
             <!--========== ISLANDS 1 ==========-->
             <section class="islands">
-                <img src="{{ asset('frontend/assets/img/index.jpg') }}" alt="" class="islands__bg" />
+                <img
+                    src="{{ asset('frontend/assets/img/index.jpg') }}"
+                    alt=""
+                    class="islands__bg"
+                />
                 <div class="bg__overlay">
                     <div class="islands__container container">
-                        <div class="islands__data" style="z-index: 99; position: relative; color: white; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">
-                            <h2 class="islands__subtitle">Telusuri</h2>
-                            <h1 class="islands__title">WISATA RELIGI</h1>
+                    <div
+                            class="islands__data"
+                            style="z-index: 99; position: relative; color: white; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;"
+                        >
+                            <h2 class="islands__subtitle">
+                                Telusuri
+                            </h2>
+                            <h1 class="islands__title">
+                                WISATA RELIGI
+                            </h1>
                             <p class="islands__description">
                                 "Waktu untuk-Nya"<br />
                                 "Di tengah kesibukan, luangkan waktu untuk-Nya yang telah memberikan segalanya."<br />
                                 "Dalam doa, kita menemukan ketenangan dan kekuatan untuk menjalani setiap hari."
                             </p>
                         </div>
+
                     </div>
                 </div>
             </section>
@@ -27,7 +39,10 @@
 </section>
 
 <!--==================== LOGOS ====================-->
-<section class="logos" style="margin-top: 9rem; padding-bottom: 3rem">
+<section
+    class="logos"
+    style="margin-top: 9rem; padding-bottom: 3rem"
+>
     <div class="logos__container container grid">
         <div class="logos__img">
             <img src="{{ asset('frontend/assets/img/sp1.jpg') }}" alt="" />
@@ -47,22 +62,34 @@
 <!--==================== POPULAR ====================-->
 <section class="section" id="popular">
     <div class="container">
-        <span class="section__subtitle" style="text-align: center">Best Choice</span>
-        <h2 class="section__title" style="text-align: center">Popular Places</h2>
+        <span class="section__subtitle" style="text-align: center"
+            >Best Choice</span
+        >
+        <h2 class="section__title" style="text-align: center">
+            Popular Places
+        </h2>
 
         <div class="popular__container swiper">
             <div class="swiper-wrapper">
                 @foreach($travel_packages as $travel_package)
-                <article class="popular__card swiper-slide">
-                    <a href="{{ route('travel_package.show', $travel_package->slug) }}">
-                        <img src="{{ Storage::url($travel_package->galleries->first()->images) }}" alt="" class="popular__img" />
-                        <div class="popular__data">
-                            <h2 class="popular__price"><span>$</span>{{ number_format($travel_package->price,2) }}</h2>
-                            <h3 class="popular__title">{{ $travel_package->location}}</h3>
-                            <p class="popular__description">{{ $travel_package->type }}</p>
-                        </div>
-                    </a>
-                </article>
+                    <article class="popular__card swiper-slide">
+                        <a href="{{ route('travel_package.show', $travel_package->slug) }}">
+                            <img
+                                src="{{ Storage::url($travel_package->galleries->first()->images) }}"
+                                alt=""
+                                class="popular__img"
+                            />
+                            <div class="popular__data">
+                                <h2 class="popular__price">
+                                    <span>$</span>{{ number_format($travel_package->price,2) }}
+                                </h2>
+                                <h3 class="popular__title">
+                                    {{ $travel_package->location}}
+                                </h3>
+                                <p class="popular__description">{{ $travel_package->type }}</p>
+                            </div>
+                        </a>
+                    </article>
                 @endforeach
             </div>
 
@@ -90,69 +117,96 @@
         <div class="value__content">
             <div class="value__data">
                 <span class="section__subtitle">Kenapa Harus Memilih Kami?</span>
-                <h2 class="section__title">Pengalaman Apa Yang Akan Anda Dapatkan</h2>
+                <h2 class="section__title">
+                    Pengalaman Apa Yang Akan Anda Dapatkan
+                </h2>
                 <p class="value__description">
-                    <b>Kami selalu siap melayani Anda dengan memberikan pelayanan terbaik. 
-                    Kami menawarkan pilihan perjalanan religi yang sempurna ke berbagai destinasi di seluruh dunia.</b>
+                <b>Kami selalu siap melayani Anda dengan memberikan pelayanan terbaik. 
+                Kami menawarkan pilihan perjalanan religi yang sempurna ke berbagai destinasi di seluruh dunia.</b>
                 </p>
             </div>
 
             <div class="value__accordion">
                 <div class="value__accordion-item">
                     <header class="value__accordion-header">
-                        <i class="bx bxs-shield-x value-accordion-icon"></i>
-                        <h3 class="value__accordion-title">Lokasi ziarah terbaik di berbagai dunia</h3>
+                        <i
+                            class="bx bxs-shield-x value-accordion-icon"
+                        ></i>
+                        <h3 class="value__accordion-title">
+                            Lokasi ziarah terbaikdiberbagai dunia 
+                        </h3>
                         <div class="value__accordion-arrow">
                             <i class="bx bxs-down-arrow"></i>
                         </div>
                     </header>
                     <div class="value__accordion-content">
-                        <ul class="value__accordion-info-list">
-                            <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Makkah" target="_blank">Mekah, Arab Saudi</a></li>
-                            <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Tanah_Lot" target="_blank">Pura Tanah Lot, Bali</a></li>
-                            <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Museum_Vatikan" target="_blank">Museum Vatican, Roma</a></li>
-                            <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Notre-Dame_de_Paris" target="_blank">Gereja Katedral Notre Dame, Paris</a></li>
-                            <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Maha_Vihara_Mojopahit" target="_blank">Maha Vihara Mojopahit, Mojokerto</a></li>
-                            <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Masjid_Baiturrahman_Banda_Aceh" target="_blank">Masjid Raya Baiturrahman, Banda Aceh</a></li>
-                        </ul>
-                    </div>
-                </div>
+    <ul class="value__accordion-info-list">
+        <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Makkah" target="_blank">Mekah, Arab Saudi</a</li>
+        <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Tanah_Lot"target="_blank">Pura Tanah Lot, Bali</a</li>
+        <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Museum_Vatikan"target="_blank">Museum Vatican, Roma</a</li>
+        <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Notre-Dame_de_Paris"target="_blank">Gereja Katedral Notre Dame, Paris</a</li>
+        <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Maha_Vihara_Mojopahit"target="_blank">Maha Vihara Mojopahit, Mojokerto</a</li>
+        <li class="value__accordion-info-item"><a href="https://id.wikipedia.org/wiki/Masjid_Baiturrahman_Banda_Aceh"target="_blank">Masjid Raya Baiturrahman,Banda aceh</a</li>
+        
+    </ul>
+</div>
                 <div class="value__accordion-item">
                     <header class="value__accordion-header">
-                        <i class="bx bxs-x-square value-accordion-icon"></i>
-                        <h3 class="value__accordion-title">Harga terbaik untuk anda</h3>
+                        <i
+                            class="bx bxs-x-square value-accordion-icon"
+                        ></i>
+                        <h3 class="value__accordion-title">
+                            Harga terbaik untuk anda
+                        </h3>
                         <div class="value__accordion-arrow">
                             <i class="bx bxs-down-arrow"></i>
                         </div>
                     </header>
+
                     <div class="value__accordion-content">
                         <p class="value__accordion-description">
-                            We try to make your budget fit with the destination that you want to go.
+                            We try to make your budget fit with the
+                            destination that you want to go.
                         </p>
                     </div>
                 </div>
                 <div class="value__accordion-item">
                     <header class="value__accordion-header">
-                        <i class="bx bxs-bar-chart-square value-accordion-icon"></i>
-                        <h3 class="value__accordion-title">Rencanakan liburan anda</h3>
+                        <i
+                            class="bx bxs-bar-chart-square value-accordion-icon"
+                        ></i>
+                        <h3 class="value__accordion-title">
+                            Rencanakan liburan anda
+                        </h3>
                         <div class="value__accordion-arrow">
                             <i class="bx bxs-down-arrow"></i>
                         </div>
                     </header>
+
                     <div class="value__accordion-content">
-                        <p class="value__accordion-description">Provides you with time properly.</p>
+                        <p class="value__accordion-description">
+                            Provides you with time properly.
+                        </p>
                     </div>
                 </div>
                 <div class="value__accordion-item">
                     <header class="value__accordion-header">
-                        <i class="bx bxs-check-square value-accordion-icon"></i>
-                        <h3 class="value__accordion-title">Jaminan keamanan</h3>
+                        <i
+                            class="bx bxs-check-square value-accordion-icon"
+                        ></i>
+                        <h3 class="value__accordion-title">
+                            Jaminan keamanan
+                        </h3>
                         <div class="value__accordion-arrow">
                             <i class="bx bxs-down-arrow"></i>
                         </div>
                     </header>
+
                     <div class="value__accordion-content">
-                        <p class="value__accordion-description">We make sure that our services have a good of security.</p>
+                        <p class="value__accordion-description">
+                            We make sure that our services have a
+                            good of security
+                        </p>
                     </div>
                 </div>
             </div>
@@ -163,23 +217,12 @@
 <!-- blog -->
 <section class="blog section" id="blog">
     <div class="blog__container container">
-        <span class="section__subtitle" style="text-align: center">Our Blog</span>
-        <h2 class="section__title" style="text-align: center">The Best Trip For You</h2>
-
-        <div class="blog__content grid">
-            @foreach($blogs as $blog)
-            <article class="blog__card">
-                <div class="blog__image">
-                    <img src="{{ Storage::url($blog->image) }}" alt="" class="blog__img" />
-                    <a href="{{ route('blog.show', $blog->slug) }}" class="blog__button">
-                        <i class="bx bx-right-arrow-alt"></i>
-                    </a>
-                </div>
-
-                <div class="blog__data">
-                    <h2 class="blog__title">{{ $blog->title }}
-                 </h2>
-
+        <span class="section__subtitle" style="text-align: center"
+            >Our Blog</span
+        >
+        <h2 class="section__title" style="text-align: center">
+            The Best Trip For You
+        </h2>
 
         <div class="blog__content grid">
             @foreach($blogs as $blog)
