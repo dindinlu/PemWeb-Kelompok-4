2bb2c0aae8d3ea416eb25105bd9f400a92d77307
Ratnacempakamanao31 <ratnamanao31@gmail.com>
2024-07-10 11:46:17 +0700

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -57,8 +57,8 @@
                   </div>
                 </div>
 
-                <button class="button contact__card-button">Call Now</button>
-              </div>
+                <a href="tel:+62882005992391" class="button contact__card-button" target="_blank">Call Now</a>
+            </div>
               <div class="contact__card-box">
                 <div class="contact__card-info">
                   <i class="bx bxs-message-rounded-dots"></i>
@@ -69,8 +69,8 @@
                   </div>
                 </div>
 
-                <button class="button contact__card-button">Chat Now</button>
-              </div>
+                <a href="https://wa.me/6281265580858" class="button contact__card-button" target="_blank">Chat Now</a>
+                </div>
               <div class="contact__card-box">
                 <div class="contact__card-info">
                   <i class="bx bxs-video"></i>
@@ -81,8 +81,8 @@
                   </div>
                 </div>
 
-                <button class="button contact__card-button">
-                  Video Call Now
+                <a href="https://wa.me/6283154680860" class="button contact__card-button" target="_blank">Video Call Now</a>
+              </div>
                 </button>
               </div>
               <div class="contact__card-box">
@@ -95,7 +95,8 @@
                   </div>
                 </div>
 
-                <button class="button contact__card-button">Message Now</button>
+                <a href="sms:+625878371521" class="button contact__card-button" target="_blank">Message Now</a>
+                </div>
               </div>
             </div>
           </div>
