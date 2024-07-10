da81a9dd106cf34419b7db096e5ba34856d8fecf
Ratnacempakamanao31 <ratnamanao31@gmail.com>
2024-07-10 11:59:06 +0700

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -51,51 +51,28 @@
                 <div class="contact__card-info">
                   <i class="bx bxs-phone-call"></i>
                   <div>
-                    <h3 class="contact__card-title">Call</h3>
+                    <h3 class="contact__card-title">Whatsapp Call</h3>
                     <p class="contact__name-description">Jirien</p>
+                    <p class="contact__name-description">Lukman</p>
                     <p class="contact__card-description">+62882005992391</p>
+                    <p class="contact__card-description">+625878371521</p>
                   </div>
                 </div>
-
                 <button class="button contact__card-button">Call Now</button>
               </div>
+
               <div class="contact__card-box">
                 <div class="contact__card-info">
                   <i class="bx bxs-message-rounded-dots"></i>
                   <div>
                     <h3 class="contact__card-title">Whatsapp</h3>
                     <p class="contact__name-description">Ratna</p>
-                    <p class="contact__card-description">+6281265580858</p>
-                  </div>
-                </div>
-
-                <button class="button contact__card-button">Chat Now</button>
-              </div>
-              <div class="contact__card-box">
-                <div class="contact__card-info">
-                  <i class="bx bxs-video"></i>
-                  <div>
-                    <h3 class="contact__card-title">Video Call</h3>
                     <p class="contact__name-description">Dinda</p>
+                    <p class="contact__card-description">+6281265580858</p>
                     <p class="contact__card-description">+6283154680860</p>
                   </div>
                 </div>
-
-                <button class="button contact__card-button">
-                  Video Call Now
-                </button>
-              </div>
-              <div class="contact__card-box">
-                <div class="contact__card-info">
-                  <i class="bx bxs-phone-call"></i>
-                  <div>
-                    <h3 class="contact__card-title">Message</h3>
-                    <p class="contact__name-description">Lukman</p>
-                    <p class="contact__card-description">+625878371521</p>
-                  </div>
-                </div>
-
-                <button class="button contact__card-button">Message Now</button>
+                <button class="button contact__card-button">Chat Now</button>
               </div>
             </div>
           </div>
